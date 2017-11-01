a493519f3f60ee041cf4dda13f275b6682e64603
mattre <mat@quorum.at>
2017-11-01 10:23:40 +0100

    updated examples

---
+++ examples/projections/mercator-set-scale.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent('mercator-set-scale', {
+    dependencies: ['geojson-texture'],
+  
+    init: function () {
+
+        const geojsonTexture = this.el.components['geojson-texture'];
+        geojsonTexture.getProjection().scale(geojsonTexture.data.canvas.width / ( 2 * Math.PI))
+    }
+})
